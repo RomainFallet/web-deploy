d29a88f3bf66df1565da06d4e20346874e5b04d0
Romain Fallet <rfallet@numee.fr>
2020-05-05 22:15:48 +0200

    Remove exit commands.

--- ubuntu18.04_configure_deploy_env.sh
+++ ubuntu18.04_configure_deploy_env.sh
@@ -1,90 +1,93 @@
 #!/bin/bash
 
+# Exit script on error
+set -e
+
 ### Set up variables
 
 # Ask email if not already set (copy and paste all stuffs between "if" and "fi" in your terminal)
 if [[ -z "${email}" ]]; then
-    read -r -p "Enter your email (needed to set up email monitoring): " email || exit 1
+    read -r -p "Enter your email (needed to set up email monitoring): " email
 fi
 
 ### SSH
 
 # Diable password authentication
-sudo sed -i'.backup' -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config  || exit 1
+sudo sed -i'.backup' -e 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
 # Keep alive client connections
 echo "
 ClientAliveInterval 120
-ClientAliveCountMax 3" | sudo tee -a /etc/ssh/sshd_config > /dev/null || exit 1
+ClientAliveCountMax 3" | sudo tee -a /etc/ssh/sshd_config > /dev/null
 
 # Restart SSH
-sudo service ssh restart || exit 1
+sudo service ssh restart
 
 ### Updates
 
 # Install latest updates
-sudo apt update && sudo apt dist-upgrade -y || exit 1
+sudo apt update && sudo apt dist-upgrade -y
 
 # Make a backup of the config files
-sudo cp /etc/apt/apt.conf.d/10periodic /etc/apt/apt.conf.d/.10periodic.backup || exit 1
-sudo cp /etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/.50unattended-upgrades.backup || exit 1
+sudo cp /etc/apt/apt.conf.d/10periodic /etc/apt/apt.conf.d/.10periodic.backup
+sudo cp /etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/.50unattended-upgrades.backup
 
 # Download updates when available
-sudo sed -i'.tmp' -e 's,APT::Periodic::Download-Upgradeable-Packages "0";,APT::Periodic::Download-Upgradeable-Packages "1";,g' /etc/apt/apt.conf.d/10periodic || exit 1
+sudo sed -i'.tmp' -e 's,APT::Periodic::Download-Upgradeable-Packages "0";,APT::Periodic::Download-Upgradeable-Packages "1";,g' /etc/apt/apt.conf.d/10periodic
 
 # Clean apt cache every week
-sudo sed -i'.tmp' -e 's,APT::Periodic::AutocleanInterval "0";,APT::Periodic::AutocleanInterval "7";,g' /etc/apt/apt.conf.d/10periodic || exit 1
+sudo sed -i'.tmp' -e 's,APT::Periodic::AutocleanInterval "0";,APT::Periodic::AutocleanInterval "7";,g' /etc/apt/apt.conf.d/10periodic
 
 # Enable automatic updates once downloaded
-sudo sed -i'.tmp' -e 's,//\s"${distro_id}:${distro_codename}-updates";,        "${distro_id}:${distro_codename}-updates";,g' /etc/apt/apt.conf.d/50unattended-upgrades || exit 1
+sudo sed -i'.tmp' -e 's,//\s"${distro_id}:${distro_codename}-updates";,        "${distro_id}:${distro_codename}-updates";,g' /etc/apt/apt.conf.d/50unattended-upgrades
 
 # Enable email notifications
-sudo sed -i'.tmp' -e "s,//Unattended-Upgrade::Mail \"root\";,Unattended-Upgrade::Mail \"${email}\";,g" /etc/apt/apt.conf.d/50unattended-upgrades || exit 1
+sudo sed -i'.tmp' -e "s,//Unattended-Upgrade::Mail \"root\";,Unattended-Upgrade::Mail \"${email}\";,g" /etc/apt/apt.conf.d/50unattended-upgrades
 
 # Enable email notifications only on failures
-sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::MailOnlyOnError "true";,Unattended-Upgrade::MailOnlyOnError "true";,g' /etc/apt/apt.conf.d/50unattended-upgrades || exit 1
+sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::MailOnlyOnError "true";,Unattended-Upgrade::MailOnlyOnError "true";,g' /etc/apt/apt.conf.d/50unattended-upgrades
 
 # Remove unused kernel packages when needed
-sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::Remove-Unused-Kernel-Packages "false";,Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";,g' /etc/apt/apt.conf.d/50unattended-upgrades || exit 1
+sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::Remove-Unused-Kernel-Packages "false";,Unattended-Upgrade::Remove-Unused-Kernel-Packages "true";,g' /etc/apt/apt.conf.d/50unattended-upgrades
 
 # Remove unused dependencies when needed
-sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::Remove-Unused-Dependencies "false";,Unattended-Upgrade::Remove-Unused-Dependencies "true";,g' /etc/apt/apt.conf.d/50unattended-upgrades || exit 1
+sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::Remove-Unused-Dependencies "false";,Unattended-Upgrade::Remove-Unused-Dependencies "true";,g' /etc/apt/apt.conf.d/50unattended-upgrades
 
 # Reboot when needed
-sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::Automatic-Reboot "false";,Unattended-Upgrade::Automatic-Reboot "true";,g' /etc/apt/apt.conf.d/50unattended-upgrades || exit 1
+sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::Automatic-Reboot "false";,Unattended-Upgrade::Automatic-Reboot "true";,g' /etc/apt/apt.conf.d/50unattended-upgrades
 
 # Set reboot time to 3 AM
-sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::Automatic-Reboot-Time "02:00";,Unattended-Upgrade::Automatic-Reboot-Time "03:00";,g' /etc/apt/apt.conf.d/50unattended-upgrades || exit 1
+sudo sed -i'.tmp' -e 's,//Unattended-Upgrade::Automatic-Reboot-Time "02:00";,Unattended-Upgrade::Automatic-Reboot-Time "03:00";,g' /etc/apt/apt.conf.d/50unattended-upgrades
 
 # Remove temporary files
-sudo rm /etc/apt/apt.conf.d/10periodic.tmp || exit 1
-sudo rm /etc/apt/apt.conf.d/50unattended-upgrades.tmp || exit 1
+sudo rm /etc/apt/apt.conf.d/10periodic.tmp
+sudo rm /etc/apt/apt.conf.d/50unattended-upgrades.tmp
 
 ### Postfix
 
 # Install
-sudo DEBIAN_FRONTEND=noninteractive apt install -y postfix mailutils || exit 1
+sudo DEBIAN_FRONTEND=noninteractive apt install -y postfix mailutils
 
 # Make a backup of the config file
-sudo cp /etc/aliases /etc/.aliases.backup || exit 1
+sudo cp /etc/aliases /etc/.aliases.backup
 
 # Forwarding System Mail to your email address
-echo "root:     ${email}" | sudo tee -a /etc/aliases > /dev/null || exit 1
-sudo newaliases || exit 1
+echo "root:     ${email}" | sudo tee -a /etc/aliases > /dev/null
+sudo newaliases
 
-postconf mail_version || exit
+postconf mail_version
 
 ### Apache 2
 
 # Install
-sudo apt install -y apache2 || exit 1
+sudo apt install -y apache2
 
 # Enable modules
-sudo a2enmod ssl || exit 1
-sudo a2enmod rewrite || exit 1
+sudo a2enmod ssl
+sudo a2enmod rewrite
 
 # Restart Apache
-sudo service apache2 restart || exit 1
+sudo service apache2 restart
 
 apache2 -v
 sudo apache2ctl -M
@@ -92,28 +95,28 @@ sudo apache2ctl -M
 ### Certbot
 
 # Add Certbot official repositories
-sudo add-apt-repository universe || exit 1
-sudo add-apt-repository -y ppa:certbot/certbot || exit 1
+sudo add-apt-repository universe
+sudo add-apt-repository -y ppa:certbot/certbot
 
 # Install
-sudo apt install -y certbot || exit 1
+sudo apt install -y certbot
 
-certbot --version || exit 1
+certbot --version
 
 ### Firewall
 
 # Add rules and activate firewall
-sudo ufw allow OpenSSH || exit 1
-sudo ufw allow Postfix || exit 1
-sudo ufw allow in "Apache Full" || exit 1
-echo 'y' | sudo ufw enable || exit 1
+sudo ufw allow OpenSSH
+sudo ufw allow Postfix
+sudo ufw allow in "Apache Full"
+echo 'y' | sudo ufw enable
 
-sudo ufw status || exit 1
+sudo ufw status
 
 ### Fail2ban
 
 # Install
-sudo apt install -y fail2ban || exit 1
+sudo apt install -y fail2ban
 
 # Add SSH configuration
 echo "[sshd]
@@ -121,7 +124,7 @@ enabled = true
 port = 22
 filter = sshd
 logpath = /var/log/auth.log
-maxretry = 3" | sudo tee -a /etc/fail2ban/jail.local > /dev/null || exit 1
+maxretry = 3" | sudo tee -a /etc/fail2ban/jail.local > /dev/null
 
 # Add Postfix configuration
 echo "
@@ -130,7 +133,7 @@ enabled  = true
 port     = smtp
 filter   = postfix
 logpath  = /var/log/mail.log
-maxretry = 5" | sudo tee -a /etc/fail2ban/jail.local > /dev/null || exit 1
+maxretry = 5" | sudo tee -a /etc/fail2ban/jail.local > /dev/null
 
 # Add Apache configuration
 echo "
@@ -187,45 +190,45 @@ maxretry = 2
 enabled = true
 port    = http,https
 filter  = php-url-fopen
-logpath = /var/log/apache*/*access.log" | sudo tee -a /etc/fail2ban/jail.local > /dev/null || exit 1
+logpath = /var/log/apache*/*access.log" | sudo tee -a /etc/fail2ban/jail.local > /dev/null
 
 # Restart Fail2ban
-sudo service fail2ban restart || exit 1
+sudo service fail2ban restart
 
-fail2ban-client -V || exit 1
-sudo fail2ban-client status || exit 1
+fail2ban-client -V
+sudo fail2ban-client status
 
 ### PHP/Symfony environment (optional)
 
 # Ask for PHP/Symfony environment
 if [[ -z "${phpsymfony}" ]]; then
-    read -r -p "Do you want to install PHP/Symfony environment? [N/y]: " phpsymfony || exit 1
-    phpsymfony=${phpsymfony:-n} || exit 1
-    phpsymfony=$(echo "${phpsymfony}" | awk '{print tolower($0)}') || exit 1
+    read -r -p "Do you want to install PHP/Symfony environment? [N/y]: " phpsymfony
+    phpsymfony=${phpsymfony:-n}
+    phpsymfony=$(echo "${phpsymfony}" | awk '{print tolower($0)}')
 fi
 
 if [[ "${phpsymfony}" == 'y' ]]; then
   # Install PHP/Symfony dev environment
-  bash -c "$(wget --no-cache -O- https://raw.githubusercontent.com/RomainFallet/symfony-dev-ubuntu/master/ubuntu18.04_configure_dev_env.sh)" || exit 1
+  bash -c "$(wget --no-cache -O- https://raw.githubusercontent.com/RomainFallet/symfony-dev-ubuntu/master/ubuntu18.04_configure_dev_env.sh)"
 
   # Get path to PHP config file
-  phpinipath=$(php -r "echo php_ini_loaded_file();") || exit 1
+  phpinipath=$(php -r "echo php_ini_loaded_file();")
 
   # Disable functions that can causes security breaches
-  sudo sed -i'.tmp' -e 's/disable_functions =/disable_functions = error_reporting,ini_set,exec,passthru,shell_exec,system,proc_open,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source/g' "${phpinipath}" || exit 1
+  sudo sed -i'.tmp' -e 's/disable_functions =/disable_functions = error_reporting,ini_set,exec,passthru,shell_exec,system,proc_open,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source/g' "${phpinipath}"
 
   # Hide errors (can cause security issues)
-  sudo sed -i'.tmp' -e 's/display_errors = On/display_errors = Off/g' "${phpinipath}" || exit 1
-  sudo sed -i'.tmp' -e 's/display_startup_errors = On/display_startup_errors = Off/g' "${phpinipath}" || exit 1
-  sudo sed -i'.tmp' -e 's/error_reporting = E_ALL/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/g' "${phpinipath}" || exit 1
+  sudo sed -i'.tmp' -e 's/display_errors = On/display_errors = Off/g' "${phpinipath}"
+  sudo sed -i'.tmp' -e 's/display_startup_errors = On/display_startup_errors = Off/g' "${phpinipath}"
+  sudo sed -i'.tmp' -e 's/error_reporting = E_ALL/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/g' "${phpinipath}"
 
   # Remove temporary file
-  sudo rm "${phpinipath}.tmp" || exit 1
+  sudo rm "${phpinipath}.tmp"
 
   # Disable Xdebug extension (can cause performance issues)
-  sudo phpdismod xdebug || exit 1
+  sudo phpdismod xdebug
 
   # Apply PHP configuration to Apache
-  sudo cp /etc/php/7.3/apache2/php.ini /etc/php/7.3/apache2/.php.ini.backup || exit 1
-  sudo mv "${phpinipath}" /etc/php/7.3/apache2/php.ini || exit 1
+  sudo cp /etc/php/7.3/apache2/php.ini /etc/php/7.3/apache2/.php.ini.backup
+  sudo mv "${phpinipath}" /etc/php/7.3/apache2/php.ini
 fi
--- ubuntu18.04_deploy_symfony_app.sh
+++ ubuntu18.04_deploy_symfony_app.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+# Exit script on error
+set -e
+
 ### Set up variables for PHP/Symfony app deployment
 
 # Ask email if not already set (copy and paste all stuffs between "if" and "fi" in your terminal)
 if [[ -z "${email}" ]]; then
-    read -r -p "Enter your email (needed to set up email monitoring): " email || exit 1
+    read -r -p "Enter your email (needed to set up email monitoring): " email
 fi
 
 # Ask app name if not already set (copy and paste all stuffs between "if" and "fi" in your terminal)
 if [[ -z "${appname}" ]]; then
-    read -r -p "Enter the name of your app without hyphens (eg. myawesomeapp): " appname || exit 1
+    read -r -p "Enter the name of your app without hyphens (eg. myawesomeapp): " appname
 fi
 
 # Ask domain name if not already set (copy and paste all stuffs between "if" and "fi" in your terminal)
 if [[ -z "${appdomain}" ]]; then
-    read -r -p "Enter the domain name on which you want your app to be served (eg. example.com or test.example.com): " appdomain || exit 1
+    read -r -p "Enter the domain name on which you want your app to be served (eg. example.com or test.example.com): " appdomain
 fi
 
 # Ask repository URL if not already set (copy and paste all stuffs from "if" to "fi" in your terminal)
 if [[ -z "${apprepositoryurl}" ]]; then
-    read -r -p "Enter the Git repository URL of your app: " apprepositoryurl || exit 1
+    read -r -p "Enter the Git repository URL of your app: " apprepositoryurl
 fi
 
 # Ask SSH password if not already set (copy and paste all stuffs from "if" to "fi" in your terminal)
@@ -30,43 +33,43 @@ fi
 ### Clone the app
 
 # Clone app repository
-sudo git clone "${apprepositoryurl}" "/var/www/${appname}" || exit 1
+sudo git clone "${apprepositoryurl}" "/var/www/${appname}"
 
 # Go inside the app directory
-cd "/var/www/${appname}" || exit 1
+cd "/var/www/${appname}"
 
 ### Set up the database and the production mode
 
 # Generate a random password for the new mysql user
-mysqlpassword=$(openssl rand -hex 15) || exit 1
+mysqlpassword=$(openssl rand -hex 15)
 
 # Create database and related user for the app and grant permissions
-sudo mysql -e "CREATE DATABASE ${appname}; || exit 1
-CREATE USER ${appname}@localhost IDENTIFIED BY '${mysqlpassword}'; || exit 1
-GRANT ALL ON ${appname}.* TO ${appname}@localhost;" || exit 1
+sudo mysql -e "CREATE DATABASE ${appname};
+CREATE USER ${appname}@localhost IDENTIFIED BY '${mysqlpassword}';
+GRANT ALL ON ${appname}.* TO ${appname}@localhost;"
 
 # Create .env.local file
-sudo cp ./.env ./.env.local || exit 1
+sudo cp ./.env ./.env.local
 
 # Set APP_ENV to "prod"
-sudo sed -i'.tmp' -e 's/APP_ENV=dev/APP_ENV=prod/g' ./.env.local || exit 1
+sudo sed -i'.tmp' -e 's/APP_ENV=dev/APP_ENV=prod/g' ./.env.local
 
 # Set mysql credentials
-sudo sed -i'.tmp' -e 's,DATABASE_URL=mysql://db_user:db_password@127.0.0.1:3306/db_name,DATABASE_URL=mysql://'"${appname}"':'"${mysqlpassword}"'@127.0.0.1:3306/'"${appname}"',g' ./.env.local || exit 1
+sudo sed -i'.tmp' -e 's,DATABASE_URL=mysql://db_user:db_password@127.0.0.1:3306/db_name,DATABASE_URL=mysql://'"${appname}"':'"${mysqlpassword}"'@127.0.0.1:3306/'"${appname}"',g' ./.env.local
 
 # Remove temporary file
-sudo rm ./.env.local.tmp || exit 1
+sudo rm ./.env.local.tmp
 
 ### Set permissions
 
 # Set ownership to Apache
-sudo chown -R www-data:www-data "/var/www/${appname}" || exit 1
+sudo chown -R www-data:www-data "/var/www/${appname}"
 
 # Set files permissions to 664
-sudo find "/var/www/${appname}" -type f -exec chmod 664 {} \; || exit 1
+sudo find "/var/www/${appname}" -type f -exec chmod 664 {} \;
 
 # Set folders permissions to 775
-sudo find "/var/www/${appname}" -type d -exec chmod 775 {} \; || exit 1
+sudo find "/var/www/${appname}" -type d -exec chmod 775 {} \;
 
 ### Set up the web server
 
@@ -77,23 +80,23 @@ echo "<VirtualHost ${appdomain}:80>
 
   # Set up document root
   DocumentRoot /var/www/${appname}/public
-</VirtualHost>" | sudo tee "/etc/apache2/sites-available/${appname}.conf" > /dev/null || exit 1
+</VirtualHost>" | sudo tee "/etc/apache2/sites-available/${appname}.conf" > /dev/null
 
 # Activate Apache conf
-sudo a2ensite "${appname}.conf" || exit 1
+sudo a2ensite "${appname}.conf"
 
 # Restart Apache to make changes available
-sudo service apache2 restart || exit 1
+sudo service apache2 restart
 
 ### Enabling HTTPS & configure for Symfony
 
 # Get a new HTTPS certficate
-sudo certbot certonly --webroot -w "/var/www/${appname}/public" -d "${appdomain}" -m "${email}" -n --agree-tos || exit 1
+sudo certbot certonly --webroot -w "/var/www/${appname}/public" -d "${appdomain}" -m "${email}" -n --agree-tos
 
 # Check certificates renewal every month
 echo '#!/bin/bash
-certbot renew' | sudo tee /etc/cron.monthly/certbot-renew.sh > /dev/null || exit 1
-sudo chmod +x /etc/cron.monthly/certbot-renew.sh || exit 1
+certbot renew' | sudo tee /etc/cron.monthly/certbot-renew.sh > /dev/null
+sudo chmod +x /etc/cron.monthly/certbot-renew.sh
 
 # Replace existing conf
 echo "<VirtualHost ${appdomain}:80>
@@ -134,53 +137,53 @@ echo "<VirtualHost ${appdomain}:80>
     SSLEngine on
     SSLCertificateFile /etc/letsencrypt/live/${appdomain}/fullchain.pem
     SSLCertificateKeyFile /etc/letsencrypt/live/${appdomain}/privkey.pem
-</VirtualHost>" | sudo tee "/etc/apache2/sites-available/${appname}.conf" > /dev/null || exit 1
+</VirtualHost>" | sudo tee "/etc/apache2/sites-available/${appname}.conf" > /dev/null
 
 # Restart Apache to make changes available
-sudo service apache2 restart || exit 1
+sudo service apache2 restart
 
 ### Create a new SSH user for the app
 
 # Encrypt the password
-sshencryptedpassword=$(echo "${sshpassword}" | openssl passwd -crypt -stdin) || exit 1
+sshencryptedpassword=$(echo "${sshpassword}" | openssl passwd -crypt -stdin)
 
 # Create the user and set the default shell
-sudo useradd -m -p "${sshencryptedpassword}" -s /bin/bash "${appname}" || exit 1
+sudo useradd -m -p "${sshencryptedpassword}" -s /bin/bash "${appname}"
 
 # Give ownership to the user
-sudo chown -R "${appname}:www-data" "/var/www/${appname}" || exit 1
+sudo chown -R "${appname}:www-data" "/var/www/${appname}"
 
 ### Create a chroot jail for this user
 
 # Create the jail
-sudo username="${appname}" use_basic_commands=n bash -c "$(wget --no-cache -O- https://raw.githubusercontent.com/RomainFallet/chroot-jail/master/create.sh)" || exit 1
+sudo username="${appname}" use_basic_commands=n bash -c "$(wget --no-cache -O- https://raw.githubusercontent.com/RomainFallet/chroot-jail/master/create.sh)"
 
 # Mount the app folder into the jail
-sudo mount --bind "/var/www/${appname}" "/home/jails/${appname}/home/${appname}" || exit 1
+sudo mount --bind "/var/www/${appname}" "/home/jails/${appname}/home/${appname}"
 
 # Make the mount permanent
-echo "/var/www/${appname} /home/jails/${appname}/home/${appname} ext4 rw,relatime,data=ordered 0 0" | sudo tee -a /etc/fstab > /dev/null || exit 1
+echo "/var/www/${appname} /home/jails/${appname}/home/${appname} ext4 rw,relatime,data=ordered 0 0" | sudo tee -a /etc/fstab > /dev/null
 
 # Clear history for security reasons
-unset HISTFILE || exit 1
-history -c || exit 1
+unset HISTFILE
+history -c
 
 ### Init or update the app
 
 # Go inside the app directory
-cd "/var/www/${appname}" || exit 1
+cd "/var/www/${appname}"
 
 # Get latest updates
-sudo su "${appname}" -c "git pull" || exit 1
+sudo su "${appname}" -c "git pull"
 
 # Install PHP dependencies
-sudo su "${appname}" -c "composer install" || exit 1
+sudo su "${appname}" -c "composer install"
 
 # Install JS dependencies if package.json is found
-sudo su "${appname}" -c "bash -c \"if [[ -f './package.json' ]]; then yarn install; fi\"" || exit 1
+sudo su "${appname}" -c "bash -c \"if [[ -f './package.json' ]]; then yarn install; fi\""
 
 # Build assets if build script is found
-sudo su "${appname}" -c "bash -c \"if [[ -f './package.json' ]]; then if grep '\"build\":' ./package.json; then yarn build; fi fi\"" || exit 1
+sudo su "${appname}" -c "bash -c \"if [[ -f './package.json' ]]; then if grep '\"build\":' ./package.json; then yarn build; fi fi\""
 
 # Execute database migrations
 sudo su "${appname}" -c "php bin/console doctrine:migrations:diff"
