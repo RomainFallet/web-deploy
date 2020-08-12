b4d4860c17ea0816a83f59d4317401b825ba6eb5
Romain Fallet <rfallet@numee.fr>
2020-08-12 20:57:07 +0200

    Fix Mariadb version.

--- ubuntu18.04_configure_deploy_env.sh
+++ ubuntu18.04_configure_deploy_env.sh
@@ -437,7 +437,7 @@ if [[ "${php}" == 'y' ]]; then
   fi
 
   # Add MariaDB official repository
-  test -f /etc/apt/sources.list.d/mariadb.list || curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo -E bash
+  test -f /etc/apt/sources.list.d/mariadb.list || curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo -E bash -s -- --mariadb-server-version=mariadb-10.4
 
   # Install
   sudo apt install -y mariadb-server-10.4
